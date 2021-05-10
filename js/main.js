a7544a28ba6fb60b0c4a33f2ae4232011d5ff5ef
Yurii <ukucher@gmail.com>
2021-05-11 01:24:16 +0300

    added fonts, texts, logo, some js and build header

--- js/main.js
+++ js/main.js
@@ -1,2 +1,33 @@
 `use strict`;
 
+/* Плавна прокрутка меню */
+let anchors = document.querySelectorAll('.menu a[href*="#"]');
+let bulbs = document.querySelectorAll('.menu .bulb');
+//console.log(anchors); //видалити
+//console.log(bulbs); //видалити
+for (anchor of anchors) {
+    if (anchor) {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            anchorId = this.getAttribute('href');
+            document.querySelector(anchorId).scrollIntoView({
+                behavior: 'smooth', block: 'start'
+            })
+        })
+    }
+}
+
+/* Стрілка вниз */
+let arrowDown = document.querySelector('.arrow-dow');
+console.log(arrowDown); //видалити
+arrowDown.addEventListener('click', function () {
+    console.log();
+    document.querySelector('#projects').scrollIntoView({
+        behavior: 'smooth', block: 'start'
+    })
+}
+)
+arrowDown.addEventListener('mouseover', function () {
+
+})
+    ;
