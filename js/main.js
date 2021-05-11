65b05699ce32af0a6441626e5a7f905b9ac3d43d
Yurii <ukucher@gmail.com>
2021-05-12 00:44:40 +0300

    added header vertical slider worked

--- js/main.js
+++ js/main.js
@@ -19,15 +19,40 @@ for (anchor of anchors) {
 
 /* Стрілка вниз */
 let arrowDown = document.querySelector('.arrow-dow');
-console.log(arrowDown); //видалити
+//console.log(arrowDown); //видалити
 arrowDown.addEventListener('click', function () {
-    console.log();
+    //console.log();
     document.querySelector('#projects').scrollIntoView({
         behavior: 'smooth', block: 'start'
     })
 }
 )
-arrowDown.addEventListener('mouseover', function () {
-
-})
+    /* arrowDown.addEventListener('mouseover', function () {
+    
+    }) */
     ;
+
+/* Слайдер вертикальний */
+let slidesV = document.querySelector('.slider-v__slides');
+let topV = 0;
+let bulbsV = document.querySelectorAll('.slider-v__pagination-item');
+console.log(bulbsV);
+
+for (let i = 0; i < bulbsV.length; i++) {
+    bulbsV[i].addEventListener('click', function () {
+        slidesV.style.top = `${topV - 100 * i}vh`;
+        for (let j = 0; j < bulbsV.length; j++) {
+            j == i ? bulbsV[j].classList.add('bulb--active') : bulbsV[j].classList.remove('bulb--active');
+        }
+    });
+}
+
+/* for (bulbV of bulbsV) {
+    bulbV.addEventListener('click', function () {
+        console.log(bulbV);
+        console.log(bulbV.scrollIntoView);
+        topV -= 100;
+        slidesV.style.top = `${topV}vh`;
+        console.log(slidesV.style.top);
+    })
+}; */
