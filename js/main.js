11164cff4b8c9c255eff21673400e87dc2edf8ee
Yurii <ukucher@gmail.com>
2021-05-12 18:15:32 +0300

    added togling bulb in top menu

--- js/main.js
+++ js/main.js
@@ -2,41 +2,60 @@
 
 /* Плавна прокрутка меню */
 let anchors = document.querySelectorAll('.menu a[href*="#"]');
-let bulbs = document.querySelectorAll('.menu .bulb');
-//console.log(anchors); //видалити
-//console.log(bulbs); //видалити
+console.log(anchors); //видалити
 for (anchor of anchors) {
     if (anchor) {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
             anchorId = this.getAttribute('href');
+            //console.log(anchorId.offsetTop);
             document.querySelector(anchorId).scrollIntoView({
-                behavior: 'smooth', block: 'start'
-            })
+                behavior: 'smooth', block: 'start',
+
+            });
+
+            /*document.querySelector(anchorId).scrolTo(anchorId.offsetTop); */
+
+            //window.scrollBy(0, -topNavigation.offsetHeight)
         })
     }
 }
+/* Тоглення маркерів пунктів меню */
+let bulbsM = document.querySelectorAll('.menu .bulb');
+window.addEventListener('scroll', function () {
+    for (anchor of anchors) {
+        anchorId = anchor.getAttribute('href');
+        section = document.querySelector(anchorId);
+        if ((window.pageYOffset + (0.9 * document.documentElement.clientHeight) >= section.offsetTop) && ((window.pageYOffset + 0.9 * document.documentElement.clientHeight) <= (section.offsetTop + section.offsetHeight))) {
+            anchor.children[0].classList.add('bulb--active');
+        } else {
+            anchor.children[0].classList.remove('bulb--active');
+        }
+    }
+})
+
+/* Зміна фона меню під час прокрутки */
+let topNavigation = document.querySelector('.top-navigation');
+let header = document.querySelector('.header');
+
+window.addEventListener('scroll', function () {
+    window.pageYOffset >= header.offsetHeight - topNavigation.offsetHeight ? topNavigation.classList.add('top-navigation--fill') : topNavigation.classList.remove('top-navigation--fill');
+}
+);
 
 /* Стрілка вниз */
 let arrowDown = document.querySelector('.arrow-dow');
-//console.log(arrowDown); //видалити
 arrowDown.addEventListener('click', function () {
-    //console.log();
     document.querySelector('#projects').scrollIntoView({
         behavior: 'smooth', block: 'start'
     })
 }
-)
-    /* arrowDown.addEventListener('mouseover', function () {
-    
-    }) */
-    ;
+);
 
 /* Слайдер вертикальний */
 let slidesV = document.querySelector('.slider-v__slides');
 let topV = 0;
 let bulbsV = document.querySelectorAll('.slider-v__pagination-item');
-console.log(bulbsV);
 
 for (let i = 0; i < bulbsV.length; i++) {
     bulbsV[i].addEventListener('click', function () {
@@ -47,12 +66,3 @@ for (let i = 0; i < bulbsV.length; i++) {
     });
 }
 
-/* for (bulbV of bulbsV) {
-    bulbV.addEventListener('click', function () {
-        console.log(bulbV);
-        console.log(bulbV.scrollIntoView);
-        topV -= 100;
-        slidesV.style.top = `${topV}vh`;
-        console.log(slidesV.style.top);
-    })
-}; */
